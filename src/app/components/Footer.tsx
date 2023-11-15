39fff72e104e7c4443abe653203c5c43efbe1533
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-11-15 19:20:14 +0100

    fix fault

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer (){
     return (
         <footer className="w-full flex mt-[100px] p-4">
             <p>
-                Toutes les pizzas sont réalisées à la main. La pate est maison avec levure fraiche et petrit au KitchenAid, Marine l&apos;aide de cuisine va donner la forme à la pate avec amour. Flo reprend la main pour la garniture et la cuisson qui est réalisé dans un four Ooni qui grâce au bois va chauffer à plus de 400° et cuire les pizzas en moins d&apos;une minute.
+                Toutes les pizzas sont réalisées à la main. La pâte est maison avec levure fraiche et petrit au KitchenAid, Marine l&apos;aide de cuisine va donner la forme à la pâte avec amour. Flo reprend la main pour la garniture et la cuisson qui est réalisée dans un four Ooni qui grâce au bois va chauffer à plus de 400° et cuire les pizzas en moins d&apos;une minute.
             </p>
         </footer>
     )
